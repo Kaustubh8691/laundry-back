34129c5f3e9002169155daa5ce916334e9c7cd29
Kaustubh8691 <kaustubhnangarepatil8691@gmail.com>
2022-12-15 18:13:07 +0530

    trail2

---
+++ app.js
@@ -0,0 +1,35 @@
+
+const express = require('express')
+const app =express()
+const dotenv = require("dotenv");
+dotenv.config();
+
+const connectDB = require("./backEnd/connection/connectdb");
+
+
+//connecting to DB
+
+connectDB(process.env.MONGO_URL);
+//external middleware
+const cors = require('cors')
+app.use(cors())
+
+
+//built-in middleware       npm remove eslint-config-react-app 
+app.use(express.json())
+
+
+//user routes
+app.use( require("./backEnd/routes/auth"))
+
+//orders routes
+app.use("/order" ,require("./backEnd/routes/orders"))
+
+
+const PORT= 5000 || process.env.PORT
+
+//creating server
+app.listen(PORT,()=>{
+    console.log("app is listing at port ",PORT);
+})
+
